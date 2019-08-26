a40b7978880d6e5a4767650e0d6a99b7284157cb
shiyingying01 <718275303@qq.com>
2019-08-26 17:08:04 +0800

    print_the_real_purchase_when_call_process

--- src/test/java/cashregister/CashRegisterTest.java
+++ src/test/java/cashregister/CashRegisterTest.java
@@ -3,6 +3,7 @@ package cashregister;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 public class CashRegisterTest {
@@ -11,22 +12,46 @@ public class CashRegisterTest {
     @Test
     public void should_print_the_real_purchase_when_call_process() {
         //given
+    	Printer printer = mock(Printer.class);
+    	Purchase purchase = mock(Purchase.class);
+    	CashRegister cashRegister = new CashRegister(printer);
+    	when(purchase.asString()).thenReturn("the message which should be print.");
+    	
         //when
+    	cashRegister.process(purchase);
+    	
         //then
+    	verify(printer).print("the message which should be print.");
     }
 
     @Test
     public void should_print_the_stub_purchase_when_call_process() {
-        //given
+    	Printer printer = mock(Printer.class);
+    	Purchase purchase = mock(Purchase.class);
+    	CashRegister cashRegister = new CashRegister(printer);
+    	//when(purchase.asString()).thenReturn("the message which should be print.");
+    	
         //when
+    	cashRegister.process(purchase);
+    	
         //then
+    	verify(purchase).asString();
     }
+   
 
     @Test
     public void should_verify_with_process_call_with_mockito() {
-        //given
+    	Printer printer = mock(Printer.class);
+    	Purchase purchase = mock(Purchase.class);
+    	CashRegister cashRegister = new CashRegister(printer);
+    	when(purchase.asString()).thenReturn("the message which should be print.");
+    	
         //when
+    	cashRegister.process(purchase);
+    	
         //then
+    	verify(printer).print("the message which should be print.");
     }
+    
 
 }
